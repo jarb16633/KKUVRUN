298c7831a36ff2a2c30eaf96555b588d284a3ecf
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2022-03-07 21:51:54 +0700

    run.dart

--- lib/screens/run.dart
+++ lib/screens/run.dart
@@ -9,36 +9,9 @@ class RunPage extends StatelessWidget {
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const SizedBox(height: 20,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: primary,
-                        borderRadius: BorderRadius.circular(50),
-                        image: const DecorationImage(
-                          image: AssetImage("asset/images/profile.png")
-                        )
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        backgroundColor: white,
+        title: "",
       ),
     );
   }
