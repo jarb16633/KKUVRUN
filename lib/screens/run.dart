ce1ec4b94ef44af232e338dab592b4c9281a9ebc
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2022-03-07 21:19:56 +0700

    profile pic update

--- lib/screens/run.dart
+++ lib/screens/run.dart
@@ -2,61 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:virtual_run_application/constants/constants.dart';
 
 class RunPage extends StatelessWidget {
-  const RunPage({Key? key}) : super(key: key);
-
+  static var screenHeight;
+  static var screenWidth;
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.only(left: appPadding, right: appPadding  / 2, top: appPadding * 2),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Padding(padding: const EdgeInsets.all(appPadding / 8),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: primary,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(appPadding / 20),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: white,
-                        shape: BoxShape.circle,
+              const SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: primary,
+                        borderRadius: BorderRadius.circular(50),
+                        image: const DecorationImage(
+                          image: AssetImage("asset/images/profile.png")
+                        )
                       ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(appPadding / 8),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage('images/profile.jpeg'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 120,),
-              const Text("Run",
-                style: TextStyle(
-                    fontFamily: "WorkSans",
-                    fontSize: 30,
-                    fontWeight: FontWeight.w900
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  shape: BoxShape.rectangle
+                    )
+                  ],
                 ),
               )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
