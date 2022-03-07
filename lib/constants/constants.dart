62de9e4f8ae8308adfd4a1fecb03655fda23bebf
jarb16633 <69179973+jarb16633@users.noreply.github.com>
2022-03-07 20:46:03 +0700

    run.dart update

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 //color
-const Color primary = Colors.deepOrange;
+const Color primary = Color(0xFFFF5722);
+const Color secondary = Color(0xFFFF8A65);
+const Color black = Colors.black;
+const Color white = Colors.white;
 
 //padding
 const double appPadding = 20.0;
--- lib/screens/run.dart
+++ lib/screens/run.dart
@@ -6,6 +6,58 @@ class RunPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(left: appPadding, right: appPadding  / 2),);
+    return Padding(padding: const EdgeInsets.only(left: appPadding, right: appPadding  / 2, top: appPadding * 2),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Padding(padding: const EdgeInsets.all(appPadding / 8),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: primary,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(appPadding / 20),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: white,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(appPadding / 8),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('images/profile.jpeg'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 120,),
+              const Text("Run",
+                style: TextStyle(
+                    fontFamily: "WorkSans",
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.rectangle
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
